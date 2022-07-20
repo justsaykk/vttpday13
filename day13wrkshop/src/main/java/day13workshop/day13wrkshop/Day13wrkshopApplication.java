d7f9a64db6899c70d004c5ab150c17b3cd3d9f41
justsaykk <kaikein.woo@gmail.com>
2022-07-20 09:55:56 +0800

    initial commit

---
+++ day13wrkshop/src/main/java/day13workshop/day13wrkshop/Day13wrkshopApplication.java
@@ -0,0 +1,13 @@
+package day13workshop.day13wrkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13wrkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13wrkshopApplication.class, args);
+	}
+
+}
---
+++ day13wrkshop/src/test/java/day13workshop/day13wrkshop/Day13wrkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package day13workshop.day13wrkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13wrkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
